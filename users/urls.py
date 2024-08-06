19406baea44ea985ff495583d528143ec2014d1f
srub <shani.rubovitch@gmail.com>
2024-08-06 16:13:29 +0300

    fix signup to login issue

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import SignUpView, profile_view
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', profile_view, name='profile'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
