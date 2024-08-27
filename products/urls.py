2aab1ac96d162add875f3da27a00f7620fd59104
srub <shani.rubovitch@gmail.com>
2024-08-27 19:40:16 +0300

    add category views and templates

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ProductUpdateView, ProductCreateView, ProductListView, ProductDetailView, ProductDeleteView
+from .views import ProductUpdateView, ProductCreateView, ProductListView, ProductDetailView, ProductDeleteView, \
+    CategoryUpdateView, CategoryCreateView, CategoryListView, CategoryDetailView, CategoryDeleteView
+
 
 urlpatterns = [
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
@@ -8,4 +10,10 @@ urlpatterns = [
     path('product/list', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='delete_product'),
+
+    path('category/update/<int:pk>/',   CategoryUpdateView.as_view(), name='update_category'),
+    path('category/create/',            CategoryCreateView.as_view(), name='create_category'),
+    path('category/list',               CategoryListView.as_view(), name='category_list'),
+    path('category/<int:pk>/',          CategoryDetailView.as_view(), name='category_detail'),
+    path('category/<int:pk>/delete',    CategoryDeleteView.as_view(), name='category_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Category
 from ecommerce.utils import SafeGetObjectMixin
 
 
@@ -82,3 +82,77 @@ class ProductDetailView(SafeGetObjectMixin, BaseProductView, DetailView):
         return super().get(request, *args, **kwargs)
 
 
+class CategoryListView(UserPassesTestMixin, ListView):
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+
+class CategoryUpdateView(UserPassesTestMixin, UpdateView):
+    model = Category
+    fields = ['description']
+    template_name = 'update_category'
+    success_url = reverse_lazy('category_list')
+
+    def test_func(self):
+        # only allow admin users
+        return self.request.user.is_staff
+
+    def form_valid(self, form):
+        # called when form was validated successfully according to model
+        # Custom form validation logic if needed
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # called when form failed validation
+        print(f"form_invalid called with form errors: {form.errors}")
+        return super().form_invalid(form)
+
+
+class CategoryCreateView(UserPassesTestMixin, CreateView):
+    model = Category
+    fields = ['name', 'description']
+    template_name = 'create_product.html'
+    success_url = reverse_lazy('category_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+    def form_invalid(self, form):
+        # Check if the form_invalid method is correctly handling errors
+        print(f"form_invalid called with form errors: {form.errors}")
+        return super().form_invalid(form)
+
+    def test_func(self):
+        # only allow admin users
+        return self.request.user.is_staff
+
+
+class BaseCategoryView(UserPassesTestMixin):
+    model = Category
+    fields = ['name', 'description']
+    success_url = reverse_lazy('category_list')
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def get_object(self, *args, **kwargs):
+        return self.model.objects.get(pk=self.kwargs['pk'])
+
+
+class CategoryDeleteView(SafeGetObjectMixin, BaseCategoryView, DeleteView):
+    template_name = 'delete_category.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class CategoryDetailView(SafeGetObjectMixin, BaseCategoryView, DetailView):
+    template_name = 'category_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
