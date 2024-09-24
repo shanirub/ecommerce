c02b0693da305efec1f8c63532a601bf049856de
srub <shani.rubovitch@gmail.com>
2024-09-24 23:25:17 +0300

    add missing static env var

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,6 +155,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
