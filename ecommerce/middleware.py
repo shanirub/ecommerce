28b28b548c8fb13d2aedf7aaf07aa4b3e34f2e8f
srub <shani.rubovitch@gmail.com>
2024-09-25 11:16:37 +0300

    fix errors issue

--- ecommerce/middleware.py
+++ ecommerce/middleware.py
@@ -1,29 +1,22 @@
-from django.http import HttpResponseServerError, HttpResponseRedirect
-from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-from django.contrib.staticfiles import finders
+from django.shortcuts import render
 import logging
+
 logger = logging.getLogger('django')
 
 
 class CustomErrorMiddleware:
-    # TODO revisit when adding permissions
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         try:
             response = self.get_response(request)
+            if response.status_code == 500:
+                return render(request, '500.html', status=500)
+            elif response.status_code == 404:
+                return render(request, '404.html', status=404)
+
+            return response
         except Exception as e:
-            # Log the error
-            logger.error(f'Error occurred: {str(e)}')
-
-            # Check for static file
-            if 'css/styles.css' in str(e):
-                # Try to serve the static file instead of returning a 500
-                return HttpResponseRedirect(settings.STATIC_URL + 'css/styles.css')
-
-            # Return a generic 500 error
-            return HttpResponseServerError("Internal Server Error")
-
-        return response
+            logger.error(f"An error occurred: {str(e)}")
+            return render(request, '500.html', status=500)
