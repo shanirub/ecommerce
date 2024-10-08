30cf36f6e69243480c635576ce3b9709f87a4688
srub <shani.rubovitch@gmail.com>
2024-10-08 17:42:41 +0300

    temp commit

--- orders/tests/test_views_functionality.py
+++ orders/tests/test_views_functionality.py
@@ -144,32 +144,110 @@ class OrderViewTest(TestCase):
 
 class OrderItemViewTest(TestCase):
     def setUp(self):
-        # create customer user
-        pass
+        # Set up permissions using assign_permissions script
+        Command().handle()
+
+        # Create a customer user
+        customer_group = Group.objects.get(name='customers')
+        self.customer_user = UserFactory(groups=[customer_group])
+
+        # Create a valid order and order item
+        self.order = OrderFactory(user=self.customer_user)
+        self.order_item = OrderItemFactory(order=self.order)
+        self.order_item_data = model_to_dict(self.order_item)
+
+        self.assertEqual(OrderItem.objects.count(), 1)
+
+        # Create invalid order item data
+        self.invalid_order_item_data = {**self.order_item_data, 'quantity': -5}
+
+        # URLs for testing
+        self.create_url = reverse('orderitem-create', args=[self.order.pk])
+        self.update_url = reverse('orderitem-update', args=[self.order.pk, self.order_item.pk])
+        self.detail_url = reverse('orderitem-detail', args=[self.order.pk, self.order_item.pk])
+        self.delete_url = reverse('orderitem-delete', args=[self.order.pk, self.order_item.pk])
+        self.list_url = reverse('orderitem-list', args=[self.order.pk])
 
     def test_create_order_item_successful(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.create_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'create_order_item.html')
+
+        response = self.client.post(self.create_url, data=self.order_item_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('order-list'))  # Adjust success_url if necessary
+        self.assertEqual(OrderItem.objects.count(), 2)
 
     def test_create_order_item_invalid_data(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.create_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'create_order_item.html')
+
+        response = self.client.post(self.create_url, data=self.invalid_order_item_data)
+        self.assertEqual(response.status_code, 200)  # Should stay on the same page due to validation errors
+        self.assertEqual(OrderItem.objects.count(), 1)  # No new order item created
 
     def test_read_order_item(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'orders/orderitem_detail.html')
 
-    def test_non_existent_order_item(self):
-        pass
+    def test_read_non_existent_order_item(self):
+        NON_EXISTENT_ORDER_ITEM_PK = 12345
+        self.client.login(username=self.customer_user.username, password='password')
+        detail_url_non_existent = self.detail_url.replace(str(self.order_item.pk), str(NON_EXISTENT_ORDER_ITEM_PK))
+
+        response = self.client.get(detail_url_non_existent)
+        self.assertEqual(response.status_code, 404)
 
     def test_update_order_item_successful(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.update_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'update_order_item.html')
+
+        response = self.client.post(self.update_url, data={'quantity': self.order_item.quantity + 1, 'price': self.order_item.price})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('order-list'))
 
     def test_update_order_item_invalid_data(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.update_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'update_order_item.html')
+
+        response = self.client.post(self.update_url, data=self.invalid_order_item_data)
+        self.assertEqual(response.status_code, 200)  # Stays on the page due to validation errors
 
     def test_update_non_existent_order_item(self):
-        pass
+        NON_EXISTENT_ORDER_ITEM_PK = 12345
+        self.client.login(username=self.customer_user.username, password='password')
+        update_url_non_existent = self.update_url.replace(str(self.order_item.pk), str(NON_EXISTENT_ORDER_ITEM_PK))
+
+        response = self.client.post(update_url_non_existent, data={'quantity': 2, 'price': 100})
+        self.assertEqual(response.status_code, 404)
 
     def test_delete_order_item_successful(self):
-        pass
+        self.client.login(username=self.customer_user.username, password='password')
+        response = self.client.get(self.delete_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delete_order_item.html')
+
+        response = self.client.post(self.delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('order-list'))
+        self.assertEqual(OrderItem.objects.count(), 0)
 
     def test_delete_non_existent_order_item(self):
-        pass
+        NON_EXISTENT_ORDER_ITEM_PK = 12345
+        self.client.login(username=self.customer_user.username, password='password')
+        delete_url_non_existent = self.delete_url.replace(str(self.order_item.pk), str(NON_EXISTENT_ORDER_ITEM_PK))
+
+        response = self.client.get(delete_url_non_existent)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(delete_url_non_existent)
+        self.assertEqual(response.status_code, 404)
